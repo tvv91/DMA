04231d1a297901c16c8b8d1138aa0928f57bbdc9
tvv91 <vladimirturovets@gmail.com>
2024-03-21 21:42:43 +0200

    Separate signalr scripts for each page

---
+++ Web/wwwroot/js/hubconnection.js
@@ -0,0 +1,10 @@
+﻿let connection = new signalR.HubConnectionBuilder().withUrl("/defaulthub").build();
+
+// connecting to signalr hub
+async function start() {
+    try {
+        await connection.start();
+    } catch (err) {
+        console.log(err);
+    }
+};
